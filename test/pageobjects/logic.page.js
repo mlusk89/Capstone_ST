e3a3fa0bb79cf10dcb95bf77a34c9399b3fc583a
mlusk89 <mlusk89@gmail.com>
2024-11-15 17:39:51 -0700

    setup complete - capcha test in progress

--- test/pageobjects/logic.page.js
+++ test/pageobjects/logic.page.js
@@ -16,11 +16,19 @@ class LogicPage extends Page {
         return $('[ng-click="ctrl.removeTab(ctrl.tab)"]');
     }
 
-    get btnDrop1() {
+    get btnProdItemDrop() {
         return $('[ng-click="$select.toggle($event)"]');
     }
 
-    get search() {
+    get btnDropCodex() {
+        return $('a#browser_toggle')
+    }
+
+    get btnItems() {
+        return $('[href="/1.0/codex/items"]')
+    }
+
+    get inputSearch() {
         return $('#queryInput');
     }
 
@@ -36,14 +44,63 @@ class LogicPage extends Page {
         return $('[href="/1.0/codex/items/ai-limiter"]')
     }
 
-    items=["Alien Protein", "Bacon Agaric", "Petroleum Coke", "Plastic", "Somersloop", "Uranium"]
-
-    async component1() {
-        await this.btnCalc.click();
-        await expect(browser.url("https://www.satisfactorytools.com/1.0/production"));
-        
+    get btnRecipes() {
+        return $(`[ng-click="ctrl.tab.tab = 'recipes'"]`)
     }
 
+    get btnAltAll() {
+        return $('[ng-click="ctrl.tab.setAllAlternateRecipes(true)"]')
+    }
+
+    get btnAltNone() {
+        return $('[ng-click="ctrl.tab.setAllAlternateRecipes(false)"]')
+    }
+
+    get btnBaseAll() {
+        return $('[ng-click="ctrl.tab.setAllBasicRecipes(true)"]')
+    }
+
+    get btnBaseNone() {
+        return $('[ng-click="ctrl.tab.setAllBasicRecipes(false)"]')
+    }
+
+    get btnSearchX() {
+        return $('.clear-filter.has-content')
+    }
+
+
+    items=["Alien Protein", "Bacon Agaric", "Petroleum Coke", "Plastic", "Somersloop", "Uranium",
+        "Bauxite", "Assembly Director System", "Alien Power Matrix", "Compacted Coal", "Computer"
+    ]
+
+    async capchaDetector() {
+        await this.btnCalc.click();
+        await expect(browser).toHaveUrl("https://www.satisfactorytools.com/1.0/production");
+        await this.btnRecipes.click();
+        await this.btnAltAll.click();
+        await this.btnAltNone.click();
+        await this.btnBaseAll.click();
+        await this.btnBaseNone.click();
+        await this.btnAddTab.click();
+        await this.btnRmvTab.click();
+        await this.btnDropCodex.click();
+        await this.btnItems.click();
+        await expect(browser).toHaveUrl("https://www.satisfactorytools.com/1.0/codex/items");
+        await this.btnAdv.click();
+        await this.chkRad.click();
+    
+    }
+    
+    async search(items) {
+        await this.inputSearch.setValue(items);
+    }
+
+    async searchLoop() {
+        for(let i = 0; i<this.items.length; i++){
+            await this.search(this.items[i])
+            await this.btnSearchX.click()
+        }
+    }
 
    
   
--- test/specs/capchaTest.js
+++ test/specs/capchaTest.js
@@ -5,7 +5,8 @@ describe('My capcha test application', () => {
         it('should test for capcha traps', async () => {
             
             await LogicPage.open()
-            await LogicPage.component1()
+            await LogicPage.capchaDetector()
+            await LogicPage.searchLoop()
             
         });
 });
