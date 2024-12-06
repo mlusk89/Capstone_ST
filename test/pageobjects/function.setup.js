759298cbdd2fa684e82b21a33aa5fd16980993e2
mlusk89 <mlusk89@gmail.com>
2024-12-06 11:07:28 -0700

    cleanup - removed unused array and captcha test

--- test/pageobjects/function.setup.js
+++ test/pageobjects/function.setup.js
@@ -172,43 +172,6 @@ class Setup extends Home {
         return $('[href="/1.0/codex/items/ai-limiter"]');
     }
 
- 
-
-
-    items = ["Adaptive Control Unit", "AI Expansion Server", "AI Limiter", "Alclad Aluminum Sheet",
-        "Alien DNA Capsule", "Alien Power Matrix", "Alien Protein", "Alumina Solution",
-        "Aluminum Casing", "Aluminum Ingot", "Aluminum Scrap", "Assembly Director System",
-        "Automated Wiring", "Ballistic Warp Drive", "Battery", "Bauxite", "Biochemical Sculptor",
-        "Biomass", "Black Powder", "Cable", "Caterium Ingot", "Caterium Ore", "Chainsaw", "Circuit Board",
-        "Cluster Nobelisk", "Coal", "Compacted Coal", "Computer", "Concrete", "Cooling System",
-        "Copper Ingot", "Copper Ore", "Copper Powder", "Copper Sheet", "Crude Oil",
-        "Crystal Oscillator", "Dark Matter Crystal", "Dark Matter Residue", "Diamonds",
-        "Dissolved Silica", "Electromagnetic Control Rod", "Empty Canister", "Empty Fluid Tank",
-        "Encased Industrial Beam", "Encased Plutonium Cell", "Encased Uranium Cell",
-        "Excited Photonic Matter", "Explosive Rebar", "Fabric", "Ficsite Ingot", "Ficsite Trigon",
-        "Ficsonium", "Ficsonium Fuel Rod", "Fuel", "Fused Modular Frame", "Gas Filter",
-        "Gas Nobelisk", "Golden Factory Cart", "Hatcher Remains", "Hazmat Suit", "Heat Sink", 
-        "Heavy Modular Frame", "Heavy Oil Residue","High-Speed Connector", "Homing Rifle Ammo", "Hoverpack", 
-        "Iodine-Infused Filter", "Ionized Fuel", "Iron Ingot", "Iron Ore", "Iron Plate", "Iron Rebar", 
-        "Iron Rod", "Limestone", "Liquid Biofuel", "Magnetic Field Generator", "Medicinal Inhaler", 
-        "Mercer Sphere","Modular Engine", "Modular Frame", "Motor", "Mycelia", "Neural-Quantum Processor", 
-        "Nitric Acid", "Nitrogen Gas", "Nobelisk", "Nobelisk Detonator", "Non-Fissile Uranium", 
-        "Nuclear Pasta", "Nuke Nobelisk", "Object Scanner", "Packaged Alumina Solution", "Packaged Fuel",
-        "Packaged Heavy Oil Residue", "Packaged Ionized Fuel", "Packaged Liquid Biofuel",
-        "Packaged Nitric Acid", "Packaged Nitrogen Gas", "Packaged Oil", "Packaged Rocket Fuel",
-        "Packaged Sulfuric Acid", "Packaged Turbofuel", "Packaged Water", "Paleberry", "Parachute",
-        "Petroleum Coke", "Plastic", "Plutonium Fuel Rod", "Plutonium Pellet", "Polymer Resin", 
-        "Portable Miner", "Power Shard", "Pressure Conversion Cube", "Pulse Nobelisk", "Quartz Crystal",
-        "Quickwire", "Radio Control Unit", "Raw Quartz", "Reanimated SAM", "Reinforced Iron Plate", 
-        "Rifle Ammo", "Rocket Fuel", "Rotor", "Rubber", "SAM Fluctuator", "Screw", "Shatter Rebar", 
-        "Silica", "Singularity Cell", "Smart Plating", "Smokeless Powder", "Solid Biofuel", 
-        "Somersloop", "Spitter Remains", "Stator", "Steel Beam", "Steel Ingot", "Steel Pipe", 
-        "Stinger Remains", "Stun Rebar", "Sulfur", "Sulfuric Acid", "Supercomputer", 
-        "Superposition Oscillator", "Thermal Propulsion Rocket", "Time Crystal", "Turbo Motor", 
-        "Turbo Rifle Ammo", "Turbofuel", "Uranium", "Uranium Fuel Rod", "Uraniuim Waste",
-        "Versatile Framework", "Water", "Wire", "Wood", "Xeno-Basher", "Xeno-Zapper", 
-        "Yellow Power Slug", "Zipline"
-    ]
 
     productionItems = ["Adaptive Control Unit", "AI Expansion Server", "AI Limiter", "Alclad Aluminum Sheet", 
         "Alien DNA Capsule", "Alien Power Matrix", "Alien Protein", "Alumina Solution", "Aluminum Casing", 
